ec8aced6ca702fc30085eedc3a6d3fcccedb1e8e
Gleb Gorelov <gorelov@haulmont.com>
2015-12-28 08:57:23 +0000

    Add ability to add selection on whole group if click on group header #PL-6502

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/grouptable/CubaGroupTableWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/grouptable/CubaGroupTableWidget.java
@@ -602,16 +602,25 @@ public class CubaGroupTableWidget extends CubaScrollTableWidget {
                         }
                         setRowFocus(this);
 
-                        if (!isSingleSelectMode()) {
-                            deselectAll();
-                            sendSelectedRows(false);
+                        if ((event.getCtrlKey() || event.getMetaKey())
+                                && !event.getAltKey() && !event.getShiftKey()) {
+                            handleRowCtrlClick(event);
+                        } else {
+                            if (!isSingleSelectMode()) {
+                                deselectAll();
+                                sendSelectedRows(false);
+                            }
+                            handleRowClick(event);
                         }
-
-                        handleRowClick(event);
                         break;
                     default:
                         break;
                 }
+            }
+
+            protected void handleRowCtrlClick(Event event) {
+                client.updateVariable(paintableId, "expandAllInGroup", getGroupKey(), true);
+                DOM.eventCancelBubble(event, true);
             }
 
             protected void handleRowClick(Event event) {
