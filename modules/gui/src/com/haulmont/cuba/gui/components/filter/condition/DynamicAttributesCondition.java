bc4a271558c922a5ea10b536de1cc826c87ebf32
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-01-19 18:26:40 +0400

    PL-6568 Filter doesn't work properly for dynamic attributes and 'is set' condition

--- modules/gui/src/com/haulmont/cuba/gui/components/filter/condition/DynamicAttributesCondition.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/filter/condition/DynamicAttributesCondition.java
@@ -178,8 +178,8 @@ public class DynamicAttributesCondition extends AbstractCondition {
         if (operator == Op.NOT_EMPTY) {
             if (BooleanUtils.isTrue((Boolean) param.getValue())) {
                 text = text.replace("not exists", "exists");
-            } else if (BooleanUtils.isFalse((Boolean) param.getValue())) {
-                text = text.replace("exists ", "not exists ");
+            } else if (BooleanUtils.isFalse((Boolean) param.getValue()) && !text.contains("not exists")) {
+                text = text.replace("exists ", "not exists");
             }
         }
     }
