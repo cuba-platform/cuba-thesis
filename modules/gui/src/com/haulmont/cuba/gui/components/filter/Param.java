7e36c7c0ff46832cd4cab0d52e8f130244aee5cb
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-01-19 17:05:16 +0400

    PL-6568 Filter doesn't work properly for dynamic attributes and 'is set' condition

--- modules/gui/src/com/haulmont/cuba/gui/components/filter/Param.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/filter/Param.java
@@ -226,9 +226,6 @@ public class Param {
                 break;
 
             case RUNTIME_ENUM:
-                value = text;
-                break;
-
             case DATATYPE:
             case UNARY:
                 Datatype datatype = Datatypes.getNN(javaClass);
@@ -305,7 +302,7 @@ public class Param {
             case ENUM:
                 return ((Enum) v).name();
             case RUNTIME_ENUM:
-                return (String) v;
+//                return (String) v;
 
             case DATATYPE:
             case UNARY:
@@ -782,6 +779,9 @@ public class Param {
     }
 
     protected Component createRuntimeEnumLookup(final ValueProperty valueProperty) {
+        if (javaClass == Boolean.class) {
+            return createBooleanField(valueProperty);
+        }
         DataService dataService = AppBeans.get(DataService.NAME);
         LoadContext context = new LoadContext(CategoryAttribute.class);
         LoadContext.Query q = context.setQueryString("select a from sys$CategoryAttribute a where a.id = :id");
