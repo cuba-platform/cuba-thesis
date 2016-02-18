c0e403a1cad6bde3a9c842c40ccf55ecb5540426
Yuriy Artamonov <artamonov@haulmont.com>
2016-02-18 17:24:26 +0400

    PL-6751 TreeTable.expandAll() works too long

--- modules/web/src/com/haulmont/cuba/web/gui/components/WebTreeTable.java
+++ modules/web/src/com/haulmont/cuba/web/gui/components/WebTreeTable.java
@@ -21,7 +21,6 @@ import com.haulmont.cuba.web.toolkit.data.TreeTableContainer;
 import com.haulmont.cuba.web.toolkit.ui.CubaTreeTable;
 import com.vaadin.data.Item;
 import com.vaadin.server.Resource;
-import org.apache.commons.lang.ObjectUtils;
 
 import java.util.*;
 
@@ -81,7 +80,7 @@ public class WebTreeTable extends WebAbstractTable<CubaTreeTable> implements Tre
 
     @Override
     public void expandAll() {
-        component.expandAll();
+        component.expandAllItems();
     }
 
     @Override
