b4dfbca318fc610fb0d24df458ab54d4c8f6a769
Gleb Gorelov <gorelov@haulmont.com>
2016-04-05 14:41:42 +0400

    PL-6920 Update caption style after caption text changed

--- modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/groupbox/CubaGroupBoxWidget.java
+++ modules/web-toolkit/src/com/haulmont/cuba/web/toolkit/ui/client/groupbox/CubaGroupBoxWidget.java
@@ -35,6 +35,7 @@ public class CubaGroupBoxWidget extends VPanel implements HasEnabled {
 
     public Element captionStartDeco = DOM.createDiv();
     public Element captionEndDeco = DOM.createDiv();
+    public Element captionTextNode;
 
     public CubaGroupBoxWidget(String primaryStyleName) {
         setStylePrimaryName(primaryStyleName);
@@ -61,7 +62,7 @@ public class CubaGroupBoxWidget extends VPanel implements HasEnabled {
 
         captionNode.insertFirst(expander);
 
-        Element captionTextNode = (Element) captionNode.getChild(1);
+        captionTextNode = (Element) captionNode.getChild(1);
         captionTextNode.setClassName(primaryStyleName + "-caption-text");
 
         DOM.sinkEvents(expander, Event.ONCLICK);
